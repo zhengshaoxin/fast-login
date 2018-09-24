1023451dced7c33eced8ef77a15bdd50a9351e16
郑少鑫 <zheng.shaoxin@foxmail.com>
2018-09-24 18:17:40 +0800

    :construction: 运行时构建

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -3,5 +3,10 @@ module.exports = {
   output: {
     path: "/Users/xin/Work/Web/fast-login/dist", // 打包后的文件存放的路径
     filename: "app.js" // 打包后输出文件的文件名
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js' // 默认 NPM 包导出的是 运行时 构建
+    }
   }
 }
