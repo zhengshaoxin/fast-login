41e165d19a3c51d9bb83190efa03214ca38666e2
郑少鑫 <zheng.shaoxin@foxmail.com>
2018-09-24 19:22:39 +0800

    :construction: html-webpack-plugin

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,8 +1,11 @@
+var config = require('../config')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-  entry: "/Users/xin/Work/Web/fast-login/src/main.js", // 程序入口文件
+  entry: './src/main.js', // 程序入口文件
   output: {
-    path: "/Users/xin/Work/Web/fast-login/dist", // 打包后的文件存放的路径
-    filename: "app.js" // 打包后输出文件的文件名
+    path: config.dev.assetsRoot, // 打包后的文件存放的路径
+    filename: "app.js" // 打包后输出文件的文件名 [name].js 则与入口文件一样
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
@@ -17,5 +20,12 @@ module.exports = {
         loader: 'vue-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
 }
