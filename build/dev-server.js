d26838bbf8913470d332d416e84588b8a95700da
郑少鑫 <zheng.shaoxin@foxmail.com>
2018-09-24 20:54:22 +0800

    :construction: express

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,6 +1,28 @@
+const express = require('express')
 const webpackConfig = require('./webpack.config')
 const webpack = require('webpack')
 
-var compiler = webpack(webpackConfig, function() {
-  console.log('webpack done');
-});
+var app = express();
+var compiler = webpack(webpackConfig)
+
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
+  publicPath: webpackConfig.output.publicPath,
+  quiet: true
+})
+
+app.use(devMiddleware);
+
+const port = 3000
+const uri = 'http://localhost:' + port
+
+devMiddleware.waitUntilValid(function () {
+  console.log('> Listening at ' + uri + '\n')
+})
+
+var server = app.listen(port);
+
+module.export = {
+  close: () => {
+    server.close();
+  }
+}
